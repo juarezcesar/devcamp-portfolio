a3ca6086d10bf6b0dcf30e81f5d2bf52b2d45baa
Juarez <juarez@cesar.etc.br>
2017-11-20 20:44:42 -0200

    Routes

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status]
 
   # GET /blogs
   # GET /blogs.json
@@ -58,6 +58,13 @@ class BlogsController < ApplicationController
       format.json { head :no_content }
     end
   end
+
+  def toggle_status
+    @blog.published? ? @blog.draft! : @blog.published!
+    
+    redirect_to blogs_url notice: 'Blog status has been updated.'
+  end
+
 
   private
     # Use callbacks to share common setup or constraints between actions.
