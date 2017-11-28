5bcf3d5d2805c6ee89165056f85eaa441da41d5b
Juarez <juarez@cesar.etc.br>
2017-11-28 14:24:45 -0200

    Code formating

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
+  devise_for :users, path: '', path_names: {sign_in: 'login',
+                                            sign_out: 'logout',
+                                            sign_up: 'register'
+                                           }
   resources :portfolios, except: [:show]
   get 'angular-items', to: 'portfolio#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
